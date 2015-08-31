f9c9e36968a33da3cc456eafe737dd52d9ac0a13
makingoff <mail@makingoff.name>
2015-08-31 23:27:38 +0300

    rewritten version

--- tabs.js
+++ tabs.js
@@ -48,25 +48,15 @@
   {
     if (typeof outer === 'undefined') outer = true;
     var width = 0;
-    width += parseFloat(getStyle(item, 'width'));
-    if (outer) {
-      width += parseInt(getStyle(item, 'paddingLeft'), 10);
-      width += parseInt(getStyle(item, 'paddingRight'), 10);
-      width += parseInt(getStyle(item, 'borderLeftWidth'), 10);
-      width += parseInt(getStyle(item, 'borderRightWidth'), 10);
-      width += parseInt(getStyle(item, 'marginRight'), 10);
-      width += parseInt(getStyle(item, 'marginLeft'), 10);
-    }
+    var params = ['width'];
+    if (outer) params = params.concat(['paddingLeft', 'paddingRight', 'borderLeftWidth', 'borderRightWidth', 'marginRight', 'marginLeft']);
+    params.map(function (rule) { width += parseFloat(getStyle(item, rule)); });
     return (isNaN(width) ? 0 : width);
   };
   var _height = function (item)
   {
     var height = 0;
-    height += parseFloat(getStyle(item, 'height'));
-    height += parseInt(getStyle(item, 'paddingTop'), 10);
-    height += parseInt(getStyle(item, 'paddingBottom'), 10);
-    height += parseInt(getStyle(item, 'borderTopWidth'), 10);
-    height += parseInt(getStyle(item, 'borderBottomWidth'), 10);
+    ['height', 'paddingTop', 'paddingBottom', 'borderTopWidth', 'borderBottomWidth', 'marginTop', 'marginBottom'].map(function (rule) { height += parseFloat(getStyle(item, rule)); });
     return (isNaN(height) ? 0 : height);
   };
   var _getOffset = function (elem)
@@ -95,36 +85,51 @@
     }
   };
 
+  var InOut = function ( k )
+  {
+    if ( ( k *= 2 ) < 1 ) return 0.5 * k * k;
+    return - 0.5 * ( --k * ( k - 2 ) - 1 );
+  }
+
+
   var elements = document.querySelectorAll('[data-tabs]');
+
   _forEach(elements, function (element)
   {
     var childs = _toArray(element.childNodes);
     var wrap = document.createElement('div');
     var wrapWidth = 0;
-    var leftIdent = 0;
     var elementHeight = _height(element);
-    var elementWidth = _width(element);
+    var elementWidth = _width(element, false);
     var rightElement = document.createElement('span');
     var leftElement = document.createElement('span');
     var elementHeight = 0;
+    var logs = document.getElementsByClassName('logs')[0];
+    var logElement;
     _forEach(childs, function (item)
     {
       wrapWidth += _width(item);
       elementHeight = Math.max(elementHeight, _height(item));
       wrap.appendChild(item);
+      logElement = document.createElement('div');
+      logElement.className = 'log';
+      logs.appendChild(logElement);
     });
+    logs = logs.getElementsByClassName('log');
     element.appendChild(wrap);
     wrap.style.width = Math.ceil(wrapWidth) + 'px';
     element.style.position = 'relative';
-    element.style.overflow = 'hidden';
+    // element.style.overflow = 'hidden';
     element.style.height = elementHeight + 'px';
     wrap.style.height = elementHeight + 'px';
     wrap.style.position = 'relative';
+    // wrap.style.marginLeft = '-100px';
 
     var rightMargin = 100;
-    var leftMargin = 300;
+    var leftMargin = 100;
     var rightBorder = elementWidth - rightMargin;
     var leftBorder = leftMargin;
+    var _transition = 30;
 
     element.appendChild(leftElement);
     leftElement.style.position = 'absolute';
@@ -147,228 +152,186 @@
       item.dataLeft = _getOffset(item).left;
       item.dataWidth = _width(item, true);
       item.dataLastLeft = 0;
+      item.isShort = item.dataWidth <= _transition;
+      item.transition = item.isShort ? item.dataWidth - item.dataWidth / 3 : _transition;
       item.showLine = false;
     });
 
-    _forEach(childs, function (item)
+    _forEach(childs, function (item, index)
     {
+      item.style.width = _width(item, false) + 'px';
       item.style.position = 'absolute';
       item.style.left = item.dataLeft + 'px';
-      item.style.width = _width(item, false) + 'px';
+      // item.style.top = index * 2 + 'px';
     });
-
-    var _log = function (a, b)
-    {
-      return Math.log(b) / Math.log(a);
-    };
-
-    var powCoeff = 5;
-    var startValue = 0;
-    var diffValue = 300;
-    var getRightIdent = function (value)
-    {
-      return _log(powCoeff, (((value - startValue) * (powCoeff - 1) / diffValue) + 1));
-    };
-
-    window.getRightIdent = getRightIdent;
-
-    // console.log(wrapWidth, rightBorder, wrapWidth - rightBorder);
-
-    var coeff = getRightIdent(wrapWidth - rightBorder) / 2.5;
-    //          ↑↑↑    возвращает от 0 до 1    ↑↑↑ ← приводим до единицы
-    // console.log('coeff', coeff);
-
-    var verticalCurrentLeft = document.getElementsByClassName('vertical-current-left')[0];
 
 //#####################
 
+    var verticalCurrentLeft = document.getElementsByClassName('vertical-current-left')[0];
+    var tabsScroll = document.getElementsByClassName('tabs__scroll')[0];
     var notSoFast;
+    var offset = 0;
+    var prevOffset = 0;
+    var currentLeft = 0;
+    var currentRight = wrapWidth;
+
+    var leftSlide = false;
+    var leftSlideTransition = 0;
+    var rightSlideTransition = 0;
+    var leftSlideOffset = 0;
+
+    var makeLog = function (index)
+    {
+      var text = '';
+      for (var i = 1; i < arguments.length; i++) {
+        text += arguments[i] + '&nbsp;&nbsp;';
+      }
+      logs[index].innerHTML = text;
+    }
+    var rightline = document.getElementsByClassName('vertical-line')[0];
+
     var _work = function ()
     {
       if (!notSoFast) {
         // notSoFast = setTimeout(function ()
         // {
           // notSoFast = false;
-          var currentLeft = 0;
-          var currentRight = 0;
-          var _left;
           var prevItem;
           var count = 5;
-          var _transition = 50;
           var minWidth = 70;
           var transition;
-          // var rightBorder = elementWidth * 1.5 / 2;
 
           var beginIndex = 1;
           var firstEntire = false;
 
-          var childsLength = childs.length;
-          _forInvEach(childs, function (item, index)
-          {
-            if (index == childs.length - 1) {
-              prevItem = item;
-              currentRight = item.dataLeft;
-              return ;
-            }
+          var leftLimit = 0;
+          var rightLimit = 0;
+          var maxShow = 100;
 
-            if (item.showLine) {
-              verticalCurrentLeft.style.left = currentRight + 'px';
-            }
-            if (item.dataWidth > _transition &&
-              currentRight - item.dataWidth + 25 > leftBorder &&
-              currentRight - item.dataWidth - _transition + 3 < leftBorder
-            ) {
-              transition = currentRight - item.dataWidth - ((currentRight - item.dataWidth - leftBorder - _transition) / 3);
-              item.style.left = transition + 'px';
-              item.className = 'tabs__item tabs__item--yellow';
-              currentRight = transition;
-              if (!firstEntire) {
-                firstEntire = true;
-                beginIndex = index;
-                currentLeft -= item.dataWidth;
-              }
-            }
-            else if (item.dataWidth < _transition &&
-              currentRight - item.dataWidth + 6 > leftBorder &&
-              currentRight - item.dataWidth - _transition < leftBorder
-            ) {
-              transition = currentRight - item.dataWidth - ((currentRight - item.dataWidth - leftBorder - _transition) / 8);
-              item.style.left = transition + 'px';
-              item.className = 'tabs__item tabs__item--pink';
-              currentRight = transition;
-              if (!firstEntire) {
-                firstEntire = true;
-                beginIndex = index;
-                currentLeft -= item.dataWidth;
-              }
-            }
-            else if (item.dataWidth < _transition &&
-              currentRight - item.dataWidth < leftBorder &&
-              currentRight > leftBorder
-            ) {
-              currentRight -= item.dataWidth + ((currentRight - item.dataWidth - leftBorder - _transition) / 8);
-              if (!firstEntire) {
-                beginIndex = index;
-                currentLeft = currentRight;
-              }
-              item.style.left = currentRight + 'px';
-              item.className = 'tabs__item tabs__item--red';
-            }
-            else if (item.dataWidth > _transition && currentRight - item.dataWidth < leftBorder) {
-              currentRight = Math.min(currentRight - _transition, leftBorder);
-              item.style.left = currentRight + 'px';
-              item.className = 'tabs__item tabs__item--blue';
-              if (!firstEntire) {
-                firstEntire = true;
-                beginIndex = index;
-                currentLeft -= item.dataWidth;
-              }
-            }
-            else if (item.dataWidth < _transition &&
-              currentRight - 2 < leftBorder
-            ) {
-              currentRight -= item.dataWidth - 10;
-              item.style.left = currentRight + 'px';
-              item.className = 'tabs__item tabs__item--blue';
-              if (!firstEntire) {
-                firstEntire = true;
-                beginIndex = index;
-                currentLeft -= item.dataWidth;
-              }
-            }
-            else {
-              currentRight -= item.dataWidth;
-              if (!firstEntire) {
-                beginIndex = index;
-                currentLeft = currentRight;
-              }
-              item.style.left = currentRight + 'px';
-              item.className = 'tabs__item tabs__item--green';
-            }
-            prevItem = item;
-          });
-
-          // console.log('beginIndex', beginIndex);
-          // console.log('currentLeft', currentLeft);
+          currentLeft = 0;
+          currentRight = wrapWidth;
 
           _forEach(childs, function (item, index)
           {
-            // return false;
-            if (index < beginIndex) {
-              return ;
-            }
-            else if (index == beginIndex) {
-              prevItem = item;
-              return ;
-            }
-            if (!prevItem) {
-              prevItem = item;
-              return ;
-            }
-
-            if (prevItem.dataWidth > minWidth && currentLeft + prevItem.dataWidth > rightBorder) {
-              if (currentLeft + prevItem.dataWidth + 10 >= rightBorder &&
-                currentLeft + prevItem.dataWidth - (_transition / 2) < rightBorder
-              ) {
-                item.style.left = currentLeft + prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 3) + 'px'
-                item.className = 'tabs__item tabs__item--red';
-                currentLeft += prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 3);
+            if (currentLeft <= offset + leftLimit - leftSlideTransition) {
+              transition = offset + leftLimit - leftSlideTransition;
+              if (offset + leftLimit + item.transition - item.dataWidth - leftSlideTransition > currentLeft) {
+                leftLimit += item.transition;
               }
-              else {
-                transition = Math.max(currentLeft + _transition, rightBorder);
-                item.style.left = transition + 'px';
-                item.className = 'tabs__item tabs__item--blue';
-                currentLeft = transition;
-              }
-            }
-            else if (prevItem.dataWidth > minWidth &&
-              currentLeft + prevItem.dataWidth - 1 <= rightBorder &&
-              currentLeft + prevItem.dataWidth + _transition > rightBorder
-            ) {
-              item.style.left = currentLeft + prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 3) + 'px'
-              currentLeft += prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 3);
-              item.className = 'tabs__item tabs__item--yellow';
-            }
-            else if (prevItem.dataWidth <= minWidth &&
-              currentLeft + prevItem.dataWidth + _transition > rightBorder &&
-              currentLeft + prevItem.dataWidth <= rightBorder
-            ) {
-              item.style.left = currentLeft + prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 8) + 'px'
-              currentLeft += prevItem.dataWidth - ((currentLeft + prevItem.dataWidth + _transition - rightBorder) / 8);
-              item.className = 'tabs__item tabs__item--pink';
-            }
-            else if (
-              prevItem.dataWidth <= minWidth &&
-              currentLeft + prevItem.dataWidth + 20 > rightBorder
-            ) {
-              item.style.left = currentLeft + prevItem.dataWidth - 8 + 'px';
-              currentLeft += prevItem.dataWidth - 8;
-              item.className = 'tabs__item tabs__item--blue';
+              item.style.left = transition + 'px';
+              item.dataLeft = transition;
+              item.className = 'tabs__item';
+              currentLeft += item.dataWidth;
             }
             else {
-              currentLeft += prevItem.dataWidth;
               item.style.left = currentLeft + 'px';
-              item.className = 'tabs__item tabs__item--green';
+              item.dataLeft = currentLeft;
+              item.className = 'tabs__item';
+              currentLeft += item.dataWidth;
             }
-            // verticalCurrentLeft.style.left = currentLeft + 'px';
+
             prevItem = item;
-            if (!--count) {
-              // return false;
+          });
+
+          if (leftLimit - leftSlideTransition >= maxShow || leftLimit - leftSlideTransition <= maxShow - 30 && leftSlideTransition > 0) {
+            leftSlideTransition += offset - prevOffset;
+          }
+
+          prevOffset = offset;
+
+          // rightline.style.left = elementWidth + offset - leftLimit + 'px';
+
+          var lastIndex;
+          leftLimit = 0;
+          rightSlideTransition = 0;
+
+          _forInvEach(childs, function (item, index)
+          {
+            prevItem = childs[index - 1];
+            if (currentRight - (item.dataWidth - item.transition) + leftLimit > elementWidth + offset && currentRight - (item.dataWidth) > elementWidth + offset - maxShow) {
+              lastIndex = index;
+              transition = currentRight - item.dataWidth - item.transition + rightLimit;
+              item.dataLeft = transition;
+              item.style.left = transition + 'px';
+              rightLimit += prevItem.dataWidth - prevItem.transition;
+              leftLimit += item.transition;
+              // item.className = 'tabs__item tabs__item--blue';
+            }
+            currentRight -= parseFloat(item.dataWidth);
+          });
+          if (leftLimit > maxShow) {
+            rightSlideTransition = leftLimit - maxShow;
+          }
+
+          rightLimit = 0;
+
+          _forEach(childs, function (item, index)
+          {
+            if (index < lastIndex) return ;
+            if (!childs[lastIndex - 1] || !item.dataLeft) return ;
+            if (index == lastIndex) {
+              transition = Math.max(elementWidth + offset - leftLimit + rightSlideTransition, childs[lastIndex - 1].dataLeft + childs[lastIndex - 1].transition);
+              item.style.left = transition + 'px';
+              rightLimit += (transition - childs[lastIndex - 1].dataLeft);
+            }
+            else {
+              transition = item.dataLeft - (item.dataLeft - childs[lastIndex - 1].dataLeft - childs[lastIndex - 1].transition) + rightLimit;
+              item.style.left = transition + 'px';
+              rightLimit += item.transition;
             }
           });
+
         // }, 100);
       }
     };
 
 //#####################
 
+    (function ()
+    {
+      var startLeft = 0;
+      var startBorderLeft;
+      var isMoving = false;
+      var wrapLeft = 0;
+      var startOffset = 0;
+      tabsScroll.addEventListener('mousedown', function (e)
+      {
+        startLeft = e.screenX;
+        startPositionLeft = parseInt(getComputedStyle(this, '')['left'], 10);
+        wrapLeft = parseInt(getComputedStyle(wrap, '')['left'], 10) || 0;
+        startOffset = - offset;
+        isMoving = true;
+      });
+      document.addEventListener('mousemove', function (e)
+      {
+        if (isMoving) {
+          tabsScroll.style.left = startPositionLeft + e.screenX - startLeft + 'px';
+          var limit = 202;
+          var val = wrapLeft + e.screenX - startLeft;
+          if (val > 0) {
+            val = 0;
+          }
+          if (-val > wrapWidth - elementWidth) {
+            val = - wrapWidth + elementWidth;
+          }
+          wrap.style.left = val + 'px';
+          offset = - startOffset - val + wrapLeft;
+          _work();
+        }
+      });
+      document.addEventListener('mouseup', function ()
+      {
+        isMoving = false;
+      });
+    })();
+
     rightElement.addEventListener('click', function (e)
     {
-      leftIdent += parseInt(elementWidth * 0.7, 10);
-      if (leftIdent > wrapWidth - elementWidth) {
-        leftIdent = wrapWidth - elementWidth;
+      offset += parseInt(elementWidth * 0.7, 10);
+      if (offset > wrapWidth - elementWidth) {
+        offset = wrapWidth - elementWidth;
       }
-      wrap.style.left = '-' + leftIdent + 'px';
+      wrap.style.left = '-' + offset + 'px';
       _work();
       e.preventDefault();
       e.stopPropagation();
@@ -401,7 +364,6 @@
       var startLeft = 0;
       var startBorderRight;
       var isMoving = false;
-      var rightline = document.getElementsByClassName('vertical-line')[0];
       rightline.addEventListener('mousedown', function (e)
       {
         startLeft = e.screenX;
@@ -419,7 +381,7 @@
       document.addEventListener('mouseup', function ()
       {
         isMoving = false;
-      })
+      });
     })();
 
     (function ()
@@ -445,7 +407,7 @@
       document.addEventListener('mouseup', function ()
       {
         isMoving = false;
-      })
+      });
     })();
 
     _forEach(childs, function (item)
